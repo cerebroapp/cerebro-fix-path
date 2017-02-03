a5f6908601da204bd6de832c520ff7912c131c58
cades kao <cadeskao@gmail.com>
2017-02-03 16:05:37 +0800

    use initialize instead of plugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 'use strict';
 const fixPath = require('fix-path')
 
-const plugin = ({term, display, actions}) => {
+const initialize = () => {
   fixPath()
-};
+}
+
+const plugin = ({term, display, actions}) => {
+}
 
 module.exports = {
+  initialize: initialize,
   fn: plugin
 }
